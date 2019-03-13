6c4bf78755a8de915703a70a8596eb77b1dc3e64
jaezuchowski <jess.zuchowski@gmail.com>
2019-03-13 13:03:33 -0700

    updates to feature2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 function Horn(horn) {
-  this.name = horn.name;
+  this.title = horn.title;
   this.image_url = horn.image_url;
   this.description = horn.description;
+  this.keyword = horn.keyword;
 }
 
 Horn.allHorns = [];
+const keywords = ['narwhal', 'rhino', 'unicorn', 'unilego', 'triceratops', 'markhor', 'mouflon', 'addax', 'chameleon', 'lizard', 'dragon'];
+
 
 Horn.prototype.render = function(){
   $('main').append('<div class="clone"></div>');
@@ -16,12 +19,26 @@ Horn.prototype.render = function(){
 
   hornClone.html(hornHtml)
 
-  hornClone.find('h2').text(this.name);
+  hornClone.find('h2').text(this.title);
   hornClone.find('img').attr('src', this.image_url);
   hornClone.find('p').text(this.description);
   hornClone.removeClass('clone');
-  hornClone.attr('class', this.name);
+  hornClone.attr('class', this.title);
+  hornClone.removeAttr('class');
+
 }
+console.log(keywords);
+
+
+keywords.forEach(keyword => {
+  $('#dropdown').append($('<option></option>').val(keyword).text(keyword));
+})
+
+
+/*var $dropdown = $('#dropdown');
+$.each(result, function(){
+  $dropdown.append($('<option />').val(.keywords)
+})*/
 
 Horn.readJson = () => {
   $.get('/data/page-1.json', 'json')
@@ -39,9 +56,16 @@ Horn.loadHorns = () => {
 
 $(() => Horn.readJson());
 
-$('select[name="animal"]').on('change', function(){
+
+$('select[title="animal"]').on('change', function(){
   let $selection = $(this).val();
   $('img').hide()
+  $('p').hide()
   $(`img[data-horn = "${$selection}"]`).show()
-}) 
+})
+
+
+/*$(document).ready(function () {
+  $('.tab-content').hide()
+})*/
 
